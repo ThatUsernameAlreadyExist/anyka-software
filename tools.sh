7d7f3b22737127ba14b39a4b3b4a7d54ff1a68ff
Alexander <ThatUsernameAlreadyExist@cock.li>
2023-06-12 14:59:17 +0200

    Cmdline utility to read or write values to ini-file.

--- tools.sh
+++ tools.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
 . ./build_helpers.sh
 
-RESULT_FILES="bin/getimage bin/setconf bin/getflag bin/files-min-recorder"
+RESULT_FILES="bin/getimage bin/setconf bin/getflag bin/files-min-recorder bin/rwconf"
 
 printBuildInfo "tools" "1.0" 
 removeMustBuildFiles ${RESULT_FILES}   
 
 cd "anyka-v4l2rtspserver-main/libv4l2cpp"
 
-rm -f getimage BINARY_PATH
-rm -f setconf BINARY_PATH
-rm -f getflag BINARY_PATH
-rm -f files-min-recorder BINARY_PATH
+rm -f getimage
+rm -f setconf
+rm -f getflag
+rm -f files-min-recorder
+rm -f rwconf
 
 ${CC} ${CPPFLAGS} -DBUILD_GETIMAGE=1         -I"inc/" -o getimage src/GetImage.cpp src/SharedMemory.cpp         ${LDFLAGS}
 ${CC} ${CPPFLAGS} -DBUILD_SETCONF=1          -I"inc/" -o setconf  src/SetSharedConfig.cpp src/SharedMemory.cpp  ${LDFLAGS}
 ${CC} ${CPPFLAGS} -DBUILD_GETFLAG=1          -I"inc/" -o getflag  src/GetFlag.cpp src/SharedMemory.cpp          ${LDFLAGS}
-${CC} ${CFLAGS} -std=c99 -DBUILD_GETRECORDEDFILES=1 -I"inc/" -o files-min-recorder src/GetRecordedFiles.c              ${LDFLAGS}
+${CC} ${CPPFLAGS} -DBUILD_RWCONFIG=1         -I"inc/" -o rwconf  src/ReadWriteConfig.cpp src/ConfigFile.cpp     ${LDFLAGS}
+${CC} ${CFLAGS} -std=c99 -DBUILD_GETRECORDEDFILES=1 -I"inc/" -o files-min-recorder src/GetRecordedFiles.c       ${LDFLAGS}
 
 ${STRIP} ./getimage
 ${STRIP} ./setconf 
 ${STRIP} ./getflag 
 ${STRIP} ./files-min-recorder 
+${STRIP} ./rwconf 
 
 mv ./getimage $BINARY_PATH
 mv ./setconf $BINARY_PATH
 mv ./getflag $BINARY_PATH
 mv ./files-min-recorder $BINARY_PATH
+mv ./rwconf $BINARY_PATH
 
 cd ..
 cd ..
