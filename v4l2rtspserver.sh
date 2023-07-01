7f4257d83259da66ec225f3f7f1936b9cc08285f
Alexander <ThatUsernameAlreadyExist@cock.li>
2023-07-01 22:12:18 +0200

    Change lib to optimized version.

--- v4l2rtspserver.sh
+++ v4l2rtspserver.sh
@@ -27,7 +27,7 @@ echo "SET(CMAKE_CXX_COMPILER ${CC})" >> $TFILE
 echo "set(CMAKE_CXX_STANDARD 11)" >> $TFILE
 echo "add_definitions(-DHAVE_ALSA -DNO_OPENSSL)" >> $TFILE
 
-export LDFLAGS="${LDFLAGS} -L${LIBRARY_PATH} -lak_mt -lakaudiocodec -lakaudiofilter -lakispsdk -lakmedialib -lakuio -lakv_encode -lmpi_aenc -lmpi_md -lmpi_osd -lmpi_venc -lplat_ai -lplat_ao -lplat_common -lplat_ipcsrv -lplat_thread -lplat_venc_cb -lplat_vi -lplat_vpss -lstdc++"
+export LDFLAGS="${LDFLAGS} -L${LIBRARY_PATH} -lak_mt -lakaudiocodec -lakaudiofilter -lakispsdk -lakmedialib -lakuio -lakv_encode -lmpi_aenc -lmpi_md -lmpi_osd -lmpi_venc_ex -lplat_ai -lplat_ao -lplat_common -lplat_ipcsrv -lplat_thread -lplat_venc_cb -lplat_vi -lplat_vpss -lstdc++"
 
 cp -a -r "libs/." "${LIBRARY_PATH}"
 
